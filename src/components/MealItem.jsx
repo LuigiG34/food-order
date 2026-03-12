143eeaa43bca8c148003388195509d468493ff91
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-12 11:06:42 +0100

    Adding MealItem component

---
+++ src/components/MealItem.jsx
@@ -0,0 +1,17 @@
+export default function MealItem({ meal : { id, name, image, price, description } }) {
+    return (
+        <li className='meal-item'>
+            <article>
+                <img src={`http://localhost:3000/${image}`} alt={name} />
+                <div>
+                    <h3>{name}</h3>
+                    <p className='meal-item-price'>{price}</p>
+                    <p className='meal-item-description'>{description}</p>
+                </div>
+                <p className='meal-item-actions'>
+                    <button>Add to cart</button>
+                </p>
+            </article>
+        </li>
+    );
+}
